2226dad864daf0e6b530a15f6bd06f4103d04272
icaroperetti <icaroperetti50@gmail.com>
2025-10-09 18:15:11 -0300

    refactor: improving typing

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,1 +1,12 @@
-export type UserSubject = ['create' | 'delete' | 'manage' | 'invite', 'User']
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('delete'),
+    z.literal('get'),
+    z.literal('update'),
+    z.literal('manage'),
+  ]),
+  z.literal('User'),
+])
+export type UserSubject = z.infer<typeof userSubject>
