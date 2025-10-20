f779d0d8044fe4f24f2b0123f1d4bda83c86588a
icaroperetti <icaroperetti50@gmail.com>
2025-10-20 15:23:15 -0300

    feat: create project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,51 +1,53 @@
-import fastifyCors from '@fastify/cors'
-import fastifySwagger from '@fastify/swagger'
-import fastifySwaggerUI from '@fastify/swagger-ui'
-import fastify from 'fastify'
+import fastifyCors from "@fastify/cors";
 import fastifyJwt from "@fastify/jwt";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastify from "fastify";
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
-} from 'fastify-type-provider-zod'
+} from "fastify-type-provider-zod";
 
-import { createAccount } from './routes/auth/create-account'
-import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
-import { getProfile } from './routes/auth/get-profile';
-import { errorHandler } from './error-handler';
-import { requestPasswordRecover } from './routes/auth/request-password-recover';
-import { resetPassword } from './routes/auth/reset-password';
-import { authenticateWithGithub } from './routes/auth/auth-with-github';
-import { env } from '@saas/env';
-import { createOrganization } from './routes/orgs/create-organization';
-import { getMembership } from './routes/orgs/get-membership';
-import { getOrganization } from './routes/orgs/get-organization';
-import { getOrganizations } from './routes/orgs/get-organizations';
-import { updateOrganization } from './routes/orgs/update-organization';
-import { shutDownOrganization } from './routes/orgs/shutdown-organization';
+import { env } from "@saas/env";
+import { errorHandler } from "./error-handler";
+import { authenticateWithGithub } from "./routes/auth/auth-with-github";
+import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
+import { createAccount } from "./routes/auth/create-account";
+import { getProfile } from "./routes/auth/get-profile";
+import { requestPasswordRecover } from "./routes/auth/request-password-recover";
+import { resetPassword } from "./routes/auth/reset-password";
+import { createOrganization } from "./routes/orgs/create-organization";
+import { getMembership } from "./routes/orgs/get-membership";
+import { getOrganization } from "./routes/orgs/get-organization";
+import { getOrganizations } from "./routes/orgs/get-organizations";
+import { shutDownOrganization } from "./routes/orgs/shutdown-organization";
+import { transferOrganization } from "./routes/orgs/transfer-organization";
+import { updateOrganization } from "./routes/orgs/update-organization";
+import { createProject } from "./routes/projects/create-projects";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.setErrorHandler(errorHandler)
+app.setErrorHandler(errorHandler);
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Next.js SaaS',
-      description: 'Full-stack SaaS with multi-tenant & RBAC.',
-      version: '1.0.0',
+      title: "Next.js SaaS",
+      description: "Full-stack SaaS with multi-tenant & RBAC.",
+      version: "1.0.0",
     },
     components: {
       securitySchemes: {
         Authorization: {
-          type: 'apiKey',
-          in: 'header',
-          name: 'Authorization',
-          description: 'JWT obtained from auth route',
+          type: "apiKey",
+          in: "header",
+          name: "Authorization",
+          description: "JWT obtained from auth route",
         },
       },
     },
@@ -56,34 +58,32 @@ app.register(fastifySwagger, {
     ],
   },
   transform: jsonSchemaTransform,
-})
+});
 
 app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
-})
+  routePrefix: "/docs",
+});
 app.register(fastifyJwt, {
-  secret:env.JWT_SECRET,
-})
+  secret: env.JWT_SECRET,
+});
 
+app.register(fastifyCors);
 
-app.register(fastifyCors)
-
-app.register(createAccount)
-app.register(authenticateWithPassword)
-app.register(getProfile)
-app.register(requestPasswordRecover)
-app.register(resetPassword)
-app.register(authenticateWithGithub)
-app.register(createOrganization)
-app.register(getMembership)
-app.register(getOrganization)
-app.register(getOrganizations)
-app.register(updateOrganization)
-app.register(shutDownOrganization)
+app.register(createAccount);
+app.register(authenticateWithPassword);
+app.register(getProfile);
+app.register(requestPasswordRecover);
+app.register(resetPassword);
+app.register(authenticateWithGithub);
+app.register(createOrganization);
+app.register(getMembership);
+app.register(getOrganization);
+app.register(getOrganizations);
+app.register(updateOrganization);
+app.register(shutDownOrganization);
+app.register(transferOrganization);
+app.register(createProject);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
-  console.log('HTTP server running!')
-})
-
-
-
+  console.log("HTTP server running!");
+});
